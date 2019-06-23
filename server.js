37f533a940ec168570e203496f0c3f7c767b0f18
Toby <tobyau9000@gmail.com>
2019-06-23 14:37:42 -0400

    define plaid routes

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const passport = require('passport');
 
 const users = require("./routes/api/users");
+const plaid = require("./routes/api/plaid");
 
 const app = express();
 
@@ -31,6 +32,7 @@ require("./config/passport")(passport);
 
 // Routes
 app.use("/api/users", users);
+app.use("/api/plaid", plaid);
 
 const port = process.env.PORT || 5000
 
