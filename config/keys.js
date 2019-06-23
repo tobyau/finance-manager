98e88bb9c904842923abb8bc96c931b4f5ca2bb1
Toby <tobyau9000@gmail.com>
2019-06-23 14:06:41 -0400

    add plaid keys and ID

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,6 @@
 module.exports = {
   mongoURI: "mongodb+srv://authDBUser:soulevans123@authcluster-sij4g.mongodb.net/test?retryWrites=true&w=majority",
-  secretOrKey: "secret"
+  secretOrKey: "secret",
+  PlaidClientdID: "5d0fb47a5a4c3e0012b150b9",
+  PlaidSecret: "2bdef7a48108ac40ca0d4489dc5ee1"
 }
